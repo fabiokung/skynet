e48ff6740f6d8dd0b2c646b6746ce01318a4026d
Jonas Lippuner <jlippuner@tapir.caltech.edu>
2017-08-22 18:00:53 -0700

    fix convert_reaclib_v1_to_v2.py

--- examples/convert_reaclib_v1_to_v2.py
+++ examples/convert_reaclib_v1_to_v2.py
@@ -2,6 +2,20 @@
 
 import sys
 
+def get_line(fin):
+  l = fin.readline()
+
+  # fix Windows line endings
+  l = l.replace("\r\n", "\n")
+
+  # fix exponential format with D instead of E
+  l = l.replace("D+", "E+")
+  l = l.replace("D-", "E-")
+  l = l.replace("d+", "e+")
+  l = l.replace("d-", "e-")
+
+  return l
+
 if len(sys.argv) != 2:
   print("Usage: %s <old REACLIB file>" % sys.argv[0])
   sys.exit(1)
@@ -9,15 +23,13 @@ if len(sys.argv) != 2:
 fin = open(sys.argv[1])
 
 while (True):
-  l = fin.readline()
-  l = l.replace("D+", "E+")
-  l = l.replace("D-", "E-")
+  l = get_line(fin)
 
   if (l == ""):
     # reached end of file
     break
 
-  if ((len(l) == 2) or (len(l) == 3)):
+  if ((len(l.strip()) == 1) or (len(l.strip()) == 2)):
     chapter = int(l)
     # skip next two lines (they should be empty)
     fin.readline()
@@ -26,14 +38,10 @@ while (True):
     sys.stdout.write("%i\n" % chapter)
     sys.stdout.write(l[0:-1].ljust(74) + "\n")
 
-    l = fin.readline()
-    l = l.replace("D+", "E+")
-    l = l.replace("D-", "E-")
+    l = get_line(fin)
     sys.stdout.write(l[0:-1].ljust(74) + "\n")
 
-    l = fin.readline()
-    l = l.replace("D+", "E+")
-    l = l.replace("D-", "E-")
+    l = get_line(fin)
     sys.stdout.write(l[0:-1].ljust(74) + "\n")
 
 fin.close()
